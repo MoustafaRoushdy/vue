f14f0c4d4ecaa2ad2d80f51c1841ee6ae8581ce8
moustafa <moustafaroushdy1@gmail.com>
2022-06-30 17:15:47 +0200

    vue labs

---
+++ lab1/src/admins.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>Admins</h1>
+  <table>
+   <tr>
+    <th>index</th>
+    <th>Name</th>
+    <th>Age</th>
+    <th>Address</th>
+    <th>Action</th>
+  </tr>
+  <tr v-for="admin,index in admins" :key="admin.name">
+    <td >{{index}}</td>
+    <td >{{admin.name}}</td>
+    <td >{{admin.age}}</td>
+    <td >{{admin.address}}</td>
+    <td ><button @click="$emit('delete',index)">Delete</button></td>
+    
+  </tr>
+
+</table>
+</template>
+
+<script>
+export default {
+    name: "adminsVue",
+    inject:['admins']
+}
+</script>
+
+<style>
+
+</style>
